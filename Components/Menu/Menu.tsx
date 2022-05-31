ce642b145afa12555eb3c9c6b6421e97bbf84d02
Ivan Khoda <ivankhoda@gmail.com>
2022-05-31 23:09:51 +0300

    Adjust webapp

--- Components/Menu/Menu.tsx
+++ Components/Menu/Menu.tsx
@@ -3,14 +3,15 @@ import { MenuItem } from "../Link/Link";
 import "./Menu.style.scss";
 //TODO make components as array
 export const Menu = () => {
-  const click = () => {
+  const click = (e: Event) => {
+    e.preventDefault();
     //@ts-ignore
-    window.Telegram.WebApp.sendData("hello duuude");
+    window.Telegram.WebApp.sendData("hello");
   };
   return (
     <ul className="Menu">
-      <MenuItem linkTo="/" text="Main" onClick={click} />
-      <MenuItem linkTo="/" text="Statistics" onClick={click} />
+      <MenuItem linkTo="/" text="Main" onClick={() => click} />
+      <MenuItem linkTo="/" text="Statistics" onClick={() => click} />
     </ul>
   );
 };
