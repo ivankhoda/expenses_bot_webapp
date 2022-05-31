7852bab2c456fa8aab1b49668d5ed7bc6df25e01
Ivan Khoda <ivankhoda@gmail.com>
2022-05-31 23:24:51 +0300

    Adjust webapp

--- Components/Main/Main.tsx
+++ Components/Main/Main.tsx
@@ -9,22 +9,17 @@ export const Main = () => {
   //@ts-ignore
   const data = window.Telegram.WebApp.initData;
   // const mainButton = window.Telegram.WebApp.MainButton;
-  const sendData = () => {
-    //@ts-ignore
 
-    //@ts-ignore
-    // window.Telegram.WebApp.sendData(data);
-    //@ts-ignore
-    window.Telegram.WebApp.sendData("privet");
-  };
+  //@ts-ignore
+  window.Telegram.WebApp.sendData("privet");
   //@ts-ignore
   window.Telegram.WebApp.sendData(data);
   return (
     <>
       <div>Here is main component</div>
 
-      <button onClick={sendData}>Test init and some other data</button>
-      <button onClick={sendData}>Test init and some other data</button>
+      <button>Test init and some other data</button>
+      <button>Test init and some other data</button>
     </>
   );
 };
